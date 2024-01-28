b7d5ac8e69184d7212fd5ce816890861e4d2977f
Gaurav <imgaurave02@gmail.com>
2024-01-28 23:34:00 +0530

    regiter user pipeline config

--- src/app.js
+++ src/app.js
@@ -15,4 +15,20 @@ app.use(express.json({limit: "16kb"})) // to limit he acceptence of json from se
 app.use(express.urlencoded({extended: true, limit: "16kb"})) // to handle the URL params like you search on google gaurav singh but in params it is gaurav+singh or sometimes it will be gaurav%20singh
 app.use(express.static("public")) // to handle fevicon type images 
 app.use(cookieParser())
+
+
+//routes import
+
+import userRouter from "./routes/user.routes.js"
+
+
+//router declaration
+
+app.use("/api/v1/users", userRouter)
+
+app.get('/', (req,res) => {
+    res.send("Hello world")
+    console.log("Hello world");
+})
+
 export {app}
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen((Number)(process.env.PORT) || 8080, () => {
         console.log(`Server is running at port : ${process.env.PORT}`);
     })
 })
